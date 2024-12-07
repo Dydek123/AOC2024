78a7cd1a6dd810295fad1ad67ae25e95531e5aac
Dydek123 <mazurkiewicz707@gmail.com>
2024-12-07 13:54:39 +0100

    Advent of code - Day 7 Part 2

--- Day1/src/day/day7/Main.java
+++ Day1/src/day/day7/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Day day7 = new Day7();
         day7.runAllTest();
-        day7.runFirstTask();
-        day7.runSecondTask();
+        day7.runAllTask();
     }
 }
